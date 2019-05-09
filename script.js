3061d6b9a993758e5e2045b1cd96fb5ce04d0275
DerBerg07 <34315653+DerBerg07@users.noreply.github.com>
2019-05-10 01:30:29 +0300

    создан объект конструктор

--- script.js
+++ script.js
@@ -47,3 +47,45 @@ console.log(calc.segmentation());
 console.log(calc.history);
 console.log(calc.multiply());
 console.log(calc.history);
+
+
+// 2) создать конструктор Car
+// свойства:
+// - обьем топливного бака
+// - расход
+// - цвет ( по умолчанию черный)
+// - марка
+// - пробег
+// - количество дверей( по умолчанию 5)
+// - run функцич которая заставляет машину ехатьв параметры передаетсься количество километров (условно)
+// остальные свойства по вашему усмотрению
+//  создать 3 машины
+//  задать параметры
+//  парау раз запустить кгт для каждой машины
+//  выести пробег каждой машины, и сколько топлива осталось
+
+function Car(capasity, spand, colour = "black", mark,
+mileage, dorsCount = 5, ){
+
+  this.capasity = capasity;
+  this.littersLeft = capasity;
+  this.spand = spand;
+  this.colour = colour;
+  this.mark = mark;
+  this.mileage = mileage;
+  this.dorsCount = dorsCount;
+  this.run = function(kilometters){
+
+    this.littersLeft = this.littersLeft - (this.spand/100 * kilometters);
+    if(this.littersLeft < 0){
+
+      console.log("километров пройдено - " + this.mileage + "Закончилось топливо, вы больше не можете проехать столько");
+
+  }else{
+    this.mileage = this.mileage + kilometters;
+  console.log("километров пройдено - " + this.mileage + "топлива осталось - " + this.littersLeft);
+  };
+
+  }
+
+}
