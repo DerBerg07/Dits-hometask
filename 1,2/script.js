e5c30d466bbc178a83002e7f32727f7b7948a754
DerBerg07 <34315653+DerBerg07@users.noreply.github.com>
2019-05-14 16:35:05 +0300

    изменил калькулятор

--- 1,2/script.js
+++ 1,2/script.js
@@ -12,25 +12,25 @@ function Calculate(first, second){
     //число 1 - число 2
   this.minus = function(){
     let result = this.first - this.secon1d;
-    this.history = this.first + "-" + this.second + "=" + result ;
+    this.history.push(this.first + "-" + this.second + "=" + result);
     return result;
   };
     //число 1 + число 2
   this.multiply = function(){
     let result = this.first * this.second;
-    this.history = this.first + "*" + this.second + "=" + result;
+    this.history.push(this.first + "*" + this.second + "=" + result);
     return result;
   };
   //число 1 / число 2
   this.segmentation = function(){
     let result = this.first / this.second;
-    this.history = this.first + "/" + this.second + "=" + result;
+    this.history.push(this.first + "/" + this.second + "=" + result);
     return result;
   };
   //найти сколько % число 1 от числа 2
 this.persent =  function(){
   let result = 100/(this.first / this.second);
-  this.history = this.second + "%" + this.first + "=" + result + "%";
+  this.history.push(this.second + "%" + this.first + "=" + result + "%");
   return result;
 }
 }
